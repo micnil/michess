71aae40d5864de2fddca78c4fbf75979b261a32a
Michael Nilsson <michael@nilsson.dev>
2022-01-06 19:31:17 +0100

    remove unused import

--- libs/react-dnd/src/lib/hooks/useDrop.ts
+++ libs/react-dnd/src/lib/hooks/useDrop.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useRef } from 'react';
 import { useDragDropContext } from './useDragDropContext';
 import { assertDefined, Position } from '@michess/common-utils';
 
