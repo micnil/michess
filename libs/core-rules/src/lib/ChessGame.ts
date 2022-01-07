d5b03d8ae94443005e17bf290e523cb582688ddc
Michael Nilsson <michael@nilsson.dev>
2022-01-07 12:20:24 +0100

    Rename incorrect variable name

--- libs/core-rules/src/lib/ChessGame.ts
+++ libs/core-rules/src/lib/ChessGame.ts
@@ -2,14 +2,15 @@ import { Color, GameState } from '@michess/core-models';
 import { Chessboard } from '@michess/core-state';
 import { IChessGame } from './model/IChessGame';
 
-export const ChessGame = (board: GameState): IChessGame => {
-  const chessboard = Chessboard(board);
+export const ChessGame = (gameState: GameState): IChessGame => {
+  const chessboard = Chessboard(gameState);
+
   return {
     ...chessboard,
-    getState: () => board,
+    getState: () => gameState,
     setOrientation: (orientation: Color) =>
       ChessGame({
-        ...board,
+        ...gameState,
         ...chessboard.setOrientation(orientation).getState(),
       }),
   };
