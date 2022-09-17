ef27dc83150a241d6aecb01b0186964e54e03a38
Michael Nilsson <michael@nilsson.dev>
2022-09-17 16:44:59 +0200

    Remove old apps

--- apps/web-chess-temp/features/header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { Logo } from './Logo';
-
-export const Header = () => {
-  return (
-    <header className="flex flex-row">
-      <Logo />
-    </header>
-  );
-};
--- apps/web-chess-temp/postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: {
-    tailwindcss: { config: './apps/web-chess/tailwind.config.js' },
-    autoprefixer: {},
-  },
-};
