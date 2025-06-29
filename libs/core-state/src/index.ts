b6db1d4e9a14ce8290b8b6a79ab76cc91ea1948a
Michael Nilsson <michael@nilsson.dev>
2025-06-29 11:42:44 +0200

    Remove unused code

--- libs/core-state/src/index.ts
+++ libs/core-state/src/index.ts
@@ -1,7 +1,5 @@
 export * from './lib/Bitboard';
-export * from './lib/BoardSquare';
 export * from './lib/Chessboard';
 export * from './lib/model/IChessboard';
 export * from './lib/model/IReadOnlyChessboard';
 export * from './lib/model/MovePayload';
-
