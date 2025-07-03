6a45cca153b59575630f39f92e4b127983175380
Michael Nilsson <michael@nilsson.dev>
2025-07-03 10:55:00 +0200

    Refactor tests

--- libs/core-state/src/lib/__tests__/Bitboard.spec.ts
+++ libs/core-state/src/lib/__tests__/Bitboard.spec.ts
@@ -1,95 +1,110 @@
 import { Bitboard } from '../Bitboard';
 
 describe('Bitboard', () => {
-  it('create an empty board by default', () => {
-    const emptyBitboard = Bitboard();
-    expect(emptyBitboard.getBitboardState()).toBe(0n);
-  });
-  it('can set first bit by index', () => {
-    const emptyBitboard = Bitboard();
-
-    const testBoard = emptyBitboard.setIndex(0);
-
-    expect(testBoard.isIndexSet(0)).toBe(true);
+  describe('constructor', () => {
+    it('create an empty board by default', () => {
+      const emptyBitboard = Bitboard();
+      expect(emptyBitboard.getBitboardState()).toBe(0n);
+    });
   });
 
-  it('can set last bit by index', () => {
-    const emptyBitboard = Bitboard();
+  describe('setIndex', () => {
+    it('can set first bit by index', () => {
+      const emptyBitboard = Bitboard();
 
-    const testBoard = emptyBitboard.setIndex(63);
+      const testBoard = emptyBitboard.setIndex(0);
 
-    expect(testBoard.isIndexSet(63)).toBe(true);
+      expect(testBoard.isIndexSet(0)).toBe(true);
+    });
+
+    it('can set last bit by index', () => {
+      const emptyBitboard = Bitboard();
+
+      const testBoard = emptyBitboard.setIndex(63);
+
+      expect(testBoard.isIndexSet(63)).toBe(true);
+    });
+
+    it('can set bits outside of chess board range', () => {
+      const emptyBitboard = Bitboard();
+
+      const testBoard = emptyBitboard.setIndex(100);
+
+      expect(testBoard.isIndexSet(100)).toBe(true);
+    });
   });
 
-  it('can clear a bit by index', () => {
-    const bit31SetBoard = Bitboard(2147483648n);
+  describe('setCoord', () => {
+    it('can set 8 bits by coordinate', () => {
+      const emptyBitboard = Bitboard();
 
-    const emptyBitboard = bit31SetBoard.clearIndex(31);
+      const testBoard = emptyBitboard
+        .setCoord('a1')
+        .setCoord('b2')
+        .setCoord('c3')
+        .setCoord('d4')
+        .setCoord('e5')
+        .setCoord('f6')
+        .setCoord('g7')
+        .setCoord('h8');
 
-    expect(emptyBitboard.getBitboardState()).toBe(0n);
-    expect(emptyBitboard.isEmpty()).toBeTruthy();
+      expect(testBoard.isCoordSet('a1')).toBeTruthy();
+      expect(testBoard.isCoordSet('b2')).toBeTruthy();
+      expect(testBoard.isCoordSet('c3')).toBeTruthy();
+      expect(testBoard.isCoordSet('d4')).toBeTruthy();
+      expect(testBoard.isCoordSet('e5')).toBeTruthy();
+      expect(testBoard.isCoordSet('f6')).toBeTruthy();
+      expect(testBoard.isCoordSet('g7')).toBeTruthy();
+      expect(testBoard.isCoordSet('h8')).toBeTruthy();
+
+      // Sample testing some empty bits just in case
+      expect(testBoard.isCoordSet('a2')).toBeFalsy();
+      expect(testBoard.isCoordSet('d5')).toBeFalsy();
+    });
   });
 
-  it('can clear bit by coordinate', () => {
-    const bit31SetBoard = Bitboard(2147483648n);
+  describe('clearIndex', () => {
+    it('can clear a bit by index', () => {
+      const bit31SetBoard = Bitboard(2147483648n);
 
-    const emptyBitboard = bit31SetBoard.clearCoord('h5');
+      const emptyBitboard = bit31SetBoard.clearIndex(31);
 
-    expect(emptyBitboard.getBitboardState()).toBe(0n);
-    expect(emptyBitboard.isEmpty()).toBeTruthy();
+      expect(emptyBitboard.getBitboardState()).toBe(0n);
+      expect(emptyBitboard.isEmpty()).toBeTruthy();
+    });
   });
 
-  it('can set bits outside of chess board range', () => {
-    const emptyBitboard = Bitboard();
+  describe('clearCoord', () => {
+    it('can clear bit by coordinate', () => {
+      const bit31SetBoard = Bitboard(2147483648n);
 
-    const testBoard = emptyBitboard.setIndex(100);
+      const emptyBitboard = bit31SetBoard.clearCoord('h5');
 
-    expect(testBoard.isIndexSet(100)).toBe(true);
+      expect(emptyBitboard.getBitboardState()).toBe(0n);
+      expect(emptyBitboard.isEmpty()).toBeTruthy();
+    });
   });
 
-  it('can set 8 bits by coordinate', () => {
-    const emptyBitboard = Bitboard();
+  describe('scanForward', () => {
+    it('can scan forward to find the least significant bit index', () => {
+      const bit31SetBoard = Bitboard(2147483648n);
 
-    const testBoard = emptyBitboard
-      .setCoord('a1')
-      .setCoord('b2')
-      .setCoord('c3')
-      .setCoord('d4')
-      .setCoord('e5')
-      .setCoord('f6')
-      .setCoord('g7')
-      .setCoord('h8');
+      expect(bit31SetBoard.scanForward()).toBe(31);
+    });
 
-    expect(testBoard.isCoordSet('a1')).toBeTruthy();
-    expect(testBoard.isCoordSet('b2')).toBeTruthy();
-    expect(testBoard.isCoordSet('c3')).toBeTruthy();
-    expect(testBoard.isCoordSet('d4')).toBeTruthy();
-    expect(testBoard.isCoordSet('e5')).toBeTruthy();
-    expect(testBoard.isCoordSet('f6')).toBeTruthy();
-    expect(testBoard.isCoordSet('g7')).toBeTruthy();
-    expect(testBoard.isCoordSet('h8')).toBeTruthy();
+    it('can scan forward to find the least significant bit index', () => {
+      const bit_31_45_50_board = Bitboard(1161086426415104n);
 
-    // Sample testing some empty bits just in case
-    expect(testBoard.isCoordSet('a2')).toBeFalsy();
-    expect(testBoard.isCoordSet('d5')).toBeFalsy();
+      expect(bit_31_45_50_board.scanForward()).toBe(31);
+    });
   });
 
-  it('can scan forward to find the least significant bit index', () => {
-    const bit31SetBoard = Bitboard(2147483648n);
+  describe('countBits', () => {
+    it('can count the amount of bits', () => {
+      const bit_31_45_50_board = Bitboard(1161086426415104n);
 
-    expect(bit31SetBoard.scanForward()).toBe(31);
-  });
-
-  it('can scan forward to find the least significant bit index', () => {
-    const bit_31_45_50_board = Bitboard(1161086426415104n);
-
-    expect(bit_31_45_50_board.scanForward()).toBe(31);
-  });
-
-  it('can count the amount of bits', () => {
-    const bit_31_45_50_board = Bitboard(1161086426415104n);
-
-    expect(bit_31_45_50_board.countBits()).toBe(3);
+      expect(bit_31_45_50_board.countBits()).toBe(3);
+    });
   });
 
   describe('getLowestSetBit', () => {
