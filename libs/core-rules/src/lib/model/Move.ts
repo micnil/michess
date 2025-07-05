590d56f50b82b81744b3603c3c024d8ea7bbdaf8
Michael Nilsson <michael@nilsson.dev>
2025-07-05 10:20:32 +0200

    Add comments to move model

--- libs/core-rules/src/lib/model/Move.ts
+++ libs/core-rules/src/lib/model/Move.ts
@@ -1,8 +1,10 @@
-import type { PieceType } from '@michess/core-models';
+import { type PieceType } from '@michess/core-models';
 
 export type Move = {
   start: number;
   target: number;
   capture: boolean;
+  // enPassant?: boolean;
+  // castling?: 'king' | 'queen';
   promotion?: PieceType;
 };
