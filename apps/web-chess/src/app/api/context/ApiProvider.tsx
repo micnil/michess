af2ab06b6149652ae6bcccbce25221642a82e90d
Michael Nilsson <michael@nilsson.dev>
2025-09-14 22:16:13 +0200

    Fix using wrong rest client

--- apps/web-chess/src/app/api/context/ApiProvider.tsx
+++ apps/web-chess/src/app/api/context/ApiProvider.tsx
@@ -1,19 +1,14 @@
 import React, { ReactNode } from 'react';
-import ky from 'ky';
-import { ApiContext } from './ApiContext';
 import { Api } from '../Api';
 import { authClient } from '../infra/authClient';
+import { restClient } from '../infra/restClient';
+import { ApiContext } from './ApiContext';
 
 interface ApiProviderProps {
   children: ReactNode;
 }
 
 export const ApiProvider: React.FC<ApiProviderProps> = ({ children }) => {
-  // Create API instances
-  const restClient = ky.create({
-    prefixUrl: 'http://localhost:3000/api', // Adjust this to your API base URL
-  });
-
   const api = Api.create(restClient, authClient);
 
   return <ApiContext.Provider value={api}>{children}</ApiContext.Provider>;
