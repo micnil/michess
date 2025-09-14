9da0f56efb5baac2bb61da4de02ce43065e6be26
Michael Nilsson <michael@nilsson.dev>
2025-09-14 11:56:13 +0200

    Change name of next app in config files

--- apps/nextjs-chess/jest.config.ts
+++ apps/nextjs-chess/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'web-chess',
+  displayName: 'nextjs-chess',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
