5c9ab7952f9e10a5c253eb2e71611fb21f392888
Michael Nilsson <michael@nilsson.dev>
2025-09-15 14:33:48 +0200

    Remove old chessboard module

--- libs/react-chessboard/src/lib/model/IChessboard.ts
+++
@@ -1,6 +0,0 @@
-import { IReadOnlyChessboard } from './IReadOnlyChessboard';
-import { MovePayload } from './MovePayload';
-
-export interface IChessboard extends IReadOnlyChessboard {
-  movePiece(movePayload: MovePayload): IChessboard;
-}
