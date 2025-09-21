ce85675fbfed112ab11d828b71e754cb102f03e5
Michael Nilsson <michael@nilsson.dev>
2025-09-21 22:16:55 +0200

    Make logo a link

--- apps/web-chess/src/app/components/Logo.tsx
+++ apps/web-chess/src/app/components/Logo.tsx
@@ -1,17 +1,20 @@
+import { Link } from '@tanstack/react-router';
 import { FC } from 'react';
 import styled from 'styled-components';
 
-const LogoContainer = styled.div`
+const LogoContainer = styled(Link)`
   display: flex;
   align-items: center;
   font-size: 1.25rem;
   padding: 0.25rem;
   gap: 0.25rem;
+  text-decoration: none;
+  color: inherit;
 `;
 
 export const Logo: FC = () => {
   return (
-    <LogoContainer>
+    <LogoContainer to="/">
       <img src="/knightv2.svg" height={32} width={32} alt="logo" />
       <span>michess</span>
     </LogoContainer>
