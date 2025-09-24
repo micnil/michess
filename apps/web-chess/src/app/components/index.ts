c0f6fe5ed694506dd5024596267cfebbaf298d68
Michael Nilsson <michael@nilsson.dev>
2025-09-24 12:12:52 +0200

    Delete unused components

--- apps/web-chess/src/app/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { Navbar } from '../features/navbar/Navbar';
-export { Button } from './Button';
-export { Container } from './Container';
-export { Logo } from './Logo';
--- apps/web-chess/src/app/features/navbar/Navbar.tsx
+++ apps/web-chess/src/app/features/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { Flex } from '@radix-ui/themes';
 import { FC, ReactNode } from 'react';
-import { Logo } from '../../components';
+import { Logo } from '../../components/Logo';
 
 type Props = {
   children?: ReactNode;
