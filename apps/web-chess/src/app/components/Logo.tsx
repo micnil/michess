9c8e20e2775f5de5b6dd0ddd7571c91894931fcc
Michael Nilsson <michael@nilsson.dev>
2025-09-27 11:30:48 +0200

    Remove gap on logo text

--- apps/web-chess/src/app/components/Logo.tsx
+++ apps/web-chess/src/app/components/Logo.tsx
@@ -5,7 +5,7 @@ import { FC } from 'react';
 export const Logo: FC = () => {
   return (
     <Reset>
-      <Flex asChild gap={'2'} align={'center'}>
+      <Flex asChild align={'center'}>
         <Link to="/">
           <img src="/chess-monky-v1.svg" height={64} width={64} alt="logo" />
           <Text size={'6'} weight={'bold'} color="amber">
