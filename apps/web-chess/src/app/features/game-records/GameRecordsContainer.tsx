f865ffe3ce338c258fbcbbdd981713b28ff02ee6
Michael Nilsson <michael@nilsson.dev>
2025-09-30 14:21:18 +0200

    Add "Your turn" column to game records

--- apps/web-chess/src/app/features/game-records/GameRecordsContainer.tsx
+++ apps/web-chess/src/app/features/game-records/GameRecordsContainer.tsx
@@ -55,9 +55,12 @@ export const GameRecordsContainer: React.FC<Props> = ({ onJoinGame }) => {
       variant: 'standard',
     },
   ];
+
+  const isNoOngoingGames =
+    !isPending && ongoingGames.length === 0 && !ongoingQueryError;
   return (
     <Card size="3" style={{ padding: '24px' }}>
-      <Tabs.Root defaultValue="ongoing">
+      <Tabs.Root defaultValue={isNoOngoingGames ? 'completed' : 'ongoing'}>
         <Flex justify="between" align="center" mb="4">
           <Heading size="4" weight="medium">
             My Games
@@ -74,29 +77,39 @@ export const GameRecordsContainer: React.FC<Props> = ({ onJoinGame }) => {
             <Tabs.Content value="ongoing"></Tabs.Content>
 
             <Tabs.Content value="completed"></Tabs.Content>
-            {ongoingGames.map((game) => (
-              <Card key={game.id} variant="surface" size="1">
-                <Flex align="center" gap="4">
-                  <Box style={{ minWidth: '120px' }}>
-                    <Text weight="medium" size="3">
-                      {game.opponent.name}
-                    </Text>
-                  </Box>
-                  <Box flexGrow="1">
-                    <Text size="2" color="gray">
-                      {game.variant}
-                    </Text>
-                  </Box>
-                  <Button
-                    size="1"
-                    onClick={() => onJoinGame?.(game.id)}
-                    variant="soft"
-                  >
-                    Join
-                  </Button>
-                </Flex>
-              </Card>
-            ))}
+            {ongoingGames.map((game) => {
+              const isPlayerTurn = game.turn === game.ownSide;
+              return (
+                <Card key={game.id} variant="surface" size="1">
+                  <Flex align="center" gap="4">
+                    <Box minWidth={'120px'}>
+                      <Text weight="medium" size="3">
+                        {game.opponent.name}
+                      </Text>
+                    </Box>
+                    <Box flexGrow={isPlayerTurn ? undefined : '1'}>
+                      <Text size="2" color="gray">
+                        {game.variant}
+                      </Text>
+                    </Box>
+                    {isPlayerTurn ? (
+                      <Box flexGrow="1">
+                        <Text size="2" color={'amber'}>
+                          {'Your turn'}
+                        </Text>
+                      </Box>
+                    ) : undefined}
+                    <Button
+                      size="1"
+                      onClick={() => onJoinGame?.(game.id)}
+                      variant="soft"
+                    >
+                      Join
+                    </Button>
+                  </Flex>
+                </Card>
+              );
+            })}
             {!ongoingQueryError && ongoingGames.length === 0 && (
               <Box
                 style={{
