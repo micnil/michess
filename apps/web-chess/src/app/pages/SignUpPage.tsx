bd0e17760c88fa0ee6bb09ff2060e700c0e07a68
Michael Nilsson <michael@nilsson.dev>
2025-10-03 22:53:24 +0200

    Fix signup page extending the view to make scrollbar

--- apps/web-chess/src/app/pages/SignUpPage.tsx
+++ apps/web-chess/src/app/pages/SignUpPage.tsx
@@ -3,7 +3,6 @@ import {
   Box,
   Button,
   Card,
-  Container,
   Flex,
   Heading,
   Link,
@@ -25,110 +24,103 @@ export const SignUpPage: React.FC = () => {
   const [error, _2] = useState('');
 
   return (
-    <Container size="1">
-      <Flex
-        direction="column"
-        align="center"
-        justify="center"
-        style={{ minHeight: '100vh', padding: '2rem 0' }}
-      >
-        <Card size="3" style={{ width: '100%', maxWidth: '400px' }}>
-          <form>
-            <Flex direction="column" gap="4">
-              <Box style={{ textAlign: 'center' }}>
-                <Heading size="6" mb="2">
-                  Sign up
-                </Heading>
-                <Text color="gray" size="2">
-                  Create your account to get started
-                </Text>
-              </Box>
+    <Flex direction="column" align="center" justify="center">
+      <Card size="4" style={{ width: '100%', maxWidth: '400px' }}>
+        <form>
+          <Flex direction="column" gap="4">
+            <Box style={{ textAlign: 'center' }}>
+              <Heading size="6" mb="2">
+                Sign up
+              </Heading>
+              <Text color="gray" size="2">
+                Create your account to get started
+              </Text>
+            </Box>
 
-              <Alert text={error} />
+            <Alert text={error} />
 
-              <Flex direction="column" gap="3">
-                <TextField.Root
-                  placeholder="Name"
-                  required
-                  disabled={isLoading}
-                />
+            <Flex direction="column" gap="3">
+              <TextField.Root
+                placeholder="Name"
+                required
+                disabled={isLoading}
+              />
 
-                <TextField.Root
-                  type="email"
-                  placeholder="Email"
-                  required
-                  disabled={isLoading}
-                />
+              <TextField.Root
+                type="email"
+                placeholder="Email"
+                required
+                disabled={isLoading}
+              />
 
-                <TextField.Root
-                  type="password"
-                  placeholder="Password"
-                  required
-                  disabled={isLoading}
-                />
+              <TextField.Root
+                type="password"
+                placeholder="Password"
+                required
+                disabled={isLoading}
+              />
 
-                <TextField.Root
-                  type="password"
-                  placeholder="Confirm Password"
-                  required
-                  disabled={isLoading}
-                />
+              <TextField.Root
+                type="password"
+                placeholder="Confirm Password"
+                required
+                disabled={isLoading}
+              />
 
-                <Button
-                  mt={'1'}
-                  type="submit"
-                  size="2"
-                  style={{ width: '100%' }}
-                  disabled={isLoading}
-                >
-                  {isLoading ? 'Creating Account...' : 'Create Account'}
-                </Button>
-              </Flex>
-
-              <Flex align="center" gap="3">
-                <Separator size="4" style={{ flex: 1 }} />
-                <Text size="1" color="gray">
-                  OR
-                </Text>
-                <Separator size="4" style={{ flex: 1 }} />
-              </Flex>
-
-              <Flex direction="column" gap="2">
-                <Button
-                  type="button"
-                  variant="outline"
-                  size="3"
-                  onClick={noop}
-                  disabled={isLoading}
-                >
-                  <GoogleIcon width={'16px'} fill="white" />
-                  <Text>Continue with Google</Text>
-                </Button>
-
-                <Button
-                  type="button"
-                  variant="outline"
-                  size="3"
-                  onClick={noop}
-                  disabled={isLoading}
-                >
-                  <FacebookIcon width={'16px'} fill="white" />
-                  <Text>Continue with Facebook</Text>
-                </Button>
-              </Flex>
-
-              <Flex align="center" justify="center" gap="2">
-                <Text size="2" color="gray">
-                  Already have an account?
-                </Text>
-                <Link asChild>
-                  <RouterLink to="/sign-in">Sign in</RouterLink>
-                </Link>
-              </Flex>
+              <Button
+                mt={'1'}
+                type="submit"
+                size="2"
+                style={{ width: '100%' }}
+                disabled={isLoading}
+              >
+                {isLoading ? 'Creating Account...' : 'Create Account'}
+              </Button>
             </Flex>
-          </form>
-        </Card>
-      </Flex>
-    </Container>
+
+            <Flex align="center" gap="3">
+              <Separator size="4" style={{ flex: 1 }} />
+              <Text size="1" color="gray">
+                OR
+              </Text>
+              <Separator size="4" style={{ flex: 1 }} />
+            </Flex>
+
+            <Flex direction="column" gap="2">
+              <Button
+                type="button"
+                variant="outline"
+                size="3"
+                onClick={noop}
+                disabled={isLoading}
+              >
+                <GoogleIcon width={'16px'} fill="white" />
+                <Text>Continue with Google</Text>
+              </Button>
+
+              <Button
+                type="button"
+                variant="outline"
+                size="3"
+                onClick={noop}
+                disabled={isLoading}
+              >
+                <FacebookIcon width={'16px'} fill="white" />
+                <Text>Continue with Facebook</Text>
+              </Button>
+            </Flex>
+
+            <Flex align="center" justify="center" gap="2">
+              <Text size="2" color="gray">
+                Already have an account?
+              </Text>
+              <Link asChild>
+                <RouterLink to="/sign-in">Sign in</RouterLink>
+              </Link>
+            </Flex>
+          </Flex>
+        </form>
+      </Card>
+    </Flex>
   );
 };
