18e131ab80f175150514fef890a5cd016fe7c33c
Michael Nilsson <michael@nilsson.dev>
2025-10-03 13:18:18 +0200

    Add sign in/up buttons to navbar

--- apps/web-chess/src/app/features/navbar/Navbar.tsx
+++ apps/web-chess/src/app/features/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
-import { Box, Flex } from '@radix-ui/themes';
+import { Box, Button, Flex } from '@radix-ui/themes';
 import { FC, ReactNode } from 'react';
+import { useAuth } from '../../api/hooks/useAuth';
 import { Logo } from '../../components/Logo';
 
 type Props = {
@@ -7,11 +8,22 @@ type Props = {
 };
 
 export const Navbar: FC<Props> = () => {
+  const { auth, isLoading } = useAuth();
   return (
-    <Flex>
+    <Flex align={'center'} gap={'2'}>
       <Box flexGrow={'1'} style={{ backgroundColor: '--gray9' }}>
         <Logo />
       </Box>
+      {!isLoading || auth?.user.isAnonymous ? (
+        <>
+          <Button size={'2'} color="amber">
+            Sign up
+          </Button>
+          <Button size={'2'} color="amber" variant={'outline'}>
+            Log in
+          </Button>
+        </>
+      ) : undefined}
     </Flex>
   );
 };
