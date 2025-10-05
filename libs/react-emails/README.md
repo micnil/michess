fddf7f63858956048c921f092e70c242107d8259
Michael Nilsson <michael@nilsson.dev>
2025-10-05 21:39:58 +0200

    Adding new lib react emails and made verify email template WIP

---
+++ libs/react-emails/README.md
@@ -0,0 +1,11 @@
+# react-emails
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build react-emails` to build the library.
+
+## Running unit tests
+
+Run `nx test react-emails` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/react-emails/src/lib/templates/VerifyEmailTemplate.tsx
@@ -0,0 +1,34 @@
+import {
+  Body,
+  Container,
+  Heading,
+  Html,
+  Link,
+  Preview,
+  render,
+  Text,
+} from '@react-email/components';
+import { FC } from 'react';
+type Props = {
+  url: string;
+};
+const Component: FC<Props> = (props) => {
+  return (
+    <Html>
+      <Preview>Verify your email</Preview>
+      <Body>
+        <Container>
+          <Heading>Verify your email</Heading>
+          <Text>Click the link below to verify your email address:</Text>
+          <Link href={props.url}>Verify Email</Link>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+export const VerifyEmailTemplate = {
+  async compile(props: Props): Promise<string> {
+    return render(<Component url={props.url} />);
+  },
+};
