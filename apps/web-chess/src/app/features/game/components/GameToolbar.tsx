fdd04b4b2ed07ed5e9e02eb3196ab956fa5693f7
Michael Nilsson <michael@nilsson.dev>
2025-10-14 11:58:51 +0200

    Add popover with resign and offer draw buttons

--- apps/web-chess/src/app/features/game/components/GameToolbar.tsx
+++ apps/web-chess/src/app/features/game/components/GameToolbar.tsx
@@ -1,4 +1,5 @@
-import { Button, Flex, IconButton } from '@radix-ui/themes';
+import { Button, Flex, IconButton, Popover } from '@radix-ui/themes';
+import { Separator } from '@radix-ui/themes/dist/cjs/components/context-menu';
 import { ArrowBigLeftIcon, ArrowBigRightIcon, MenuIcon } from 'lucide-react';
 import { FC } from 'react';
 
@@ -16,9 +17,30 @@ export const GameToolbar: FC = () => {
       >
         <ArrowBigLeftIcon />
       </Button>
-      <IconButton size={'3'} variant="soft" color="gray" mx={'4'} radius="full">
-        <MenuIcon />
-      </IconButton>
+      <Popover.Root>
+        <Popover.Trigger>
+          <IconButton
+            size={'3'}
+            variant="soft"
+            color="gray"
+            mx={'4'}
+            radius="full"
+          >
+            <MenuIcon />
+          </IconButton>
+        </Popover.Trigger>
+        <Popover.Content size="3">
+          <Flex direction="column" gap="2" align={'start'}>
+            <Button size="3" variant="ghost" color="gold">
+              Offer draw
+            </Button>
+            <Separator />
+            <Button size="3" variant="ghost" color="tomato">
+              Resign
+            </Button>
+          </Flex>
+        </Popover.Content>
+      </Popover.Root>
       <Button
         size={'3'}
         variant="ghost"
