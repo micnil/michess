c510924f0a4669d2f8940e53601639d86d6dea54
Michael Nilsson <michael@nilsson.dev>
2025-10-15 12:22:02 +0200

    Add Faro to web app

--- apps/web-chess/src/main.tsx
+++ apps/web-chess/src/main.tsx
@@ -1,8 +1,24 @@
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 import '@radix-ui/themes/styles.css';
 import { RouterProvider } from '@tanstack/react-router';
 import * as ReactDOM from 'react-dom/client';
 import './global.css';
 import { router } from './router';
+
+initializeFaro({
+  url: import.meta.env.VITE_FARO_URL || '',
+  sessionTracking: {
+    samplingRate: 0.8,
+  },
+  app: {
+    name: 'web-app',
+    version: '1.0.0',
+    environment: import.meta.env.MODE ?? 'development',
+  },
+
+  instrumentations: [...getWebInstrumentations(), new TracingInstrumentation()],
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
