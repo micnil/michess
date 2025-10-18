bf41d7e9f48050888f482bf05c10ec44cfdc3431
Michael Nilsson <michael@nilsson.dev>
2025-10-18 15:24:31 +0200

    Add dev dockerfile

--- docker/postgres/init/01-init.sql
+++
@@ -1,9 +0,0 @@
--- This file will be executed when the PostgreSQL container starts for the first time
--- You can add any initial database setup here
-
--- Example: Create additional databases if needed
--- CREATE DATABASE michess_test;
-
--- Example: Create extensions
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
